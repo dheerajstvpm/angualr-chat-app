b8a9c25ed17042e126996279218b24e663a872d9
Dheeraj S <dheerajstvpm@gmail.com>
2023-02-22 14:20:27 +0530

    chat app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { ChatService } from "./services/chat.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'chatApp';
+    title = "Socket.IO chat"
+    sender = ''
+    receiver = ''
+    message = ''
+    messageArray: Array<{ sender: string, receiver: string, message: string }> = []
+    chatWindow: boolean=false;
+    isSender: boolean=false;
+
+    constructor(private chatService: ChatService) {
+        this.chatService.newUserJoined()
+            .subscribe()
+        this.chatService.userLeft()
+            .subscribe()
+        this.chatService.messageReceived()
+            .subscribe(data => {
+                if(this.sender===data.message)this.isSender=true;
+                this.messageArray.push(data)})
+    }
+
+    join() {
+        this.chatService.joinRoom({ sender: this.sender, receiver: this.receiver })
+        this.chatWindow=true;
+    }
+    leave() {
+        this.chatService.leaveRoom({ sender: this.sender, receiver: this.receiver })
+        this.chatWindow=false;
+    }
+    send() {
+        this.chatService.sendMessage({ sender: this.sender, receiver: this.receiver, message: this.message })
+        this.message = ''
+    }
 }
---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+]
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
